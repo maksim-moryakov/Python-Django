8129e4f2d0f01ca84fe4f7cda0f477cc180363f6
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-02-15 15:47:53 +0300

    Незначительные правки и добавлены комментарии

--- metanit/metanit/urls.py
+++ metanit/metanit/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+
 from django.urls import path, re_path
+
 from hello import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    # re_path позволяет задать адреса URL с помощью регулярных выражений.
+    # view: функция-представление, которое обрабатывает запрос
+    # kwargs: дополнительные аргументы, которые передаются в функцию-представление
     re_path(r'^about', views.about, kwargs={"name":"Maks", "age": 38}),
     re_path(r'^contact', views.contact),
     path('admin/', admin.site.urls),
+    # Общие маршруты должны идти в конце
+    # name: название маршрута
+    path('', views.index, name='home'),
 ]
